077736d92078699902e6b3facc65392f1bf917f6
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-05-31 20:48:26 +0530

    work on registe modal

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+const RegisterModal = () => {
+  return (
+    <div>RegisterModal</div>
+  )
+}
+
+export default RegisterModal
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import ClientOnly from './components/ClientOnly'
-import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
 import Navbar from './components/navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ClientOnly>
-          <Modal actionLabel='Submit' isOpen title='hello' secondaryActionLabel="No"/>
+          <RegisterModal/>
           <Navbar />
         </ClientOnly>
         {children}</body>

2c7c66863f473130865c3db6a14c93fe708adaf8
sn247776 <62455271+sn247776@users.noreply.github.com>
2023-06-09 22:08:51 +0530

    work on login section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
+import getCurrentUser from './actions/getCurrentUser'
 import ClientOnly from './components/ClientOnly'
+import LoginModal from './components/modals/LoginModal'
 import RegisterModal from './components/modals/RegisterModal'
 import Navbar from './components/navbar/Navbar'
 import ToasterProvider from './components/providers/ToasterProvider'
@@ -12,18 +14,20 @@ export const metadata = {
   description: 'Lets Travel all the world with Traevy',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={inter.className}>
         <ClientOnly>
           <ToasterProvider/>
+          <LoginModal/>
           <RegisterModal/>
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}</body>
     </html>

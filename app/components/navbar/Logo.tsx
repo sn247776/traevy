bc783b053ae09d504baf218ea416627e60c9b337
sn247776 <62455271+sn247776@users.noreply.github.com>
2023-06-21 21:03:34 +0530

    category added

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -7,6 +7,7 @@ const Logo = () => {
   return (
     <div>
         <Image 
+        onClick={()=> router.push('/')}
         alt="Logo"
         className="hidden md:block cursor-pointer"
         height="100"

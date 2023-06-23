b6dd0541ca25f03e5c0eeb11fd239dba2de2b87f
sn247776 <62455271+sn247776@users.noreply.github.com>
2023-06-23 11:23:23 +0530

    logo fix

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -10,7 +10,7 @@ const Logo = () => {
     <Image
       onClick={() => router.push('/')}
       className="hidden md:block cursor-pointer" 
-      src="/images/logo.png" 
+      src="https://res.cloudinary.com/di7jdhpqh/image/upload/v1687499349/Logo_gcvzmx.png" 
       height="100" 
       width="150" 
       alt="Logo" 

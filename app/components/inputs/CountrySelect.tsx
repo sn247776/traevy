4515eba628c530fc7f691e89247268650dc84ded
sn247776 <62455271+sn247776@users.noreply.github.com>
2023-06-23 13:47:42 +0530

    color fix

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -54,7 +54,7 @@ const CountrySelect: React.FC<CountrySelectProps> = ({
           colors: {
             ...theme.colors,
             primary: 'black',
-            primary25: '#ffe4e6'
+            primary25: '#8a3bdc'
           }
         })}
       />
